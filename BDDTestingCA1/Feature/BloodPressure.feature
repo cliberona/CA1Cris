3f0eccf2410086e8e49169d01347bc66d0d83521
Cristian <c.liberona@gmail.com>
2021-12-08 20:04:06 +0000

    Adding BDD testing

---
+++ BDDTestingCA1/Feature/BloodPressure.feature
@@ -0,0 +1,9 @@
+﻿Feature: BloodPressure
+	Simple test for calculate blood pressure
+
+@unitest
+Scenario: Blood pressure
+	Given the systolic pressure is 100
+	And the diastolic pressure is 60
+	When I press calculate
+	Then the result should be 'Ideal Blood Pressure'
---
+++ BDDTestingCA1/Steps/BloodPressureSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using BPCalculator;
+using NUnit.Framework;
+
+namespace BDDTestingCA1.Steps
+{
+    [Binding]
+    public class BloodPressureSteps
+    {
+        private BloodPressure bptesting = new BloodPressure();
+
+        [Given(@"the systolic pressure is (.*)")]
+        public void GivenTheSystolicPressureIs(int p0)
+        {
+            bptesting.Systolic = p0;
+            
+        }
+        
+        [Given(@"the diastolic pressure is (.*)")]
+        public void GivenTheDiastolicPressureIs(int p0)
+        {
+            bptesting.Diastolic = p0;
+        }
+        
+        [When(@"I press calculate")]
+        public void WhenIPressCalculate()
+        {
+            bptesting = new BloodPressure() { Systolic = bptesting.Systolic, Diastolic = bptesting.Diastolic };
+        }
+        
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(string p0)
+        {
+            Assert.AreEqual(bptesting.Category, BPCategory.Ideal);
+        }
+    }
+}
